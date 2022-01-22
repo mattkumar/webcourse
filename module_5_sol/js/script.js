baac14bf87a24bebadc11070bab94e6c84f146ab
mattkumar <mattkumar@gmail.com>
2022-01-22 15:09:55 -0500

    retesting sol 5

--- module_5_sol/js/script.js
+++ module_5_sol/js/script.js
@@ -116,8 +116,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-            var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
-            console.log(homeHtmlToInsertIntoMainPage)
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+      
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
